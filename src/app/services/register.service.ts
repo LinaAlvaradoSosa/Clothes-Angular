62212c35f04e87a62faefdbe555668524e224dfa
linaA <hellolinita@gmail.com>
2025-04-28 14:30:55 -0500

    correccion link

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RegisterService {
-    apiUrl: String = "https://calm-savannah-05009-2b9edf3ccbff.herokuapp.com/"
+    apiUrl: String = "https://calm-savannah-05009-2b9edf3ccbff.herokuapp.com"
     token:any = sessionStorage.getItem('token')
   constructor(private http : HttpClient) { }
 
